0279200fbd92676db8223a96c6023b28232330c3
Wardell Chandler <wardell.chandler@gmail.com>
2015-11-18 10:06:32 -0600

    Refactors to use Gradle Adds in Tasker support Adds in stronger ability to disable proximity sensor. Adds in ability to change status without restarting.

---
+++ app/src/main/java/com/mrchandler/disableprox/XposedMod.java
@@ -0,0 +1,228 @@
+package com.mrchandler.disableprox;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.os.Build;
+import android.os.Handler;
+import android.util.SparseArray;
+
+import com.mrchandler.disableprox.util.Constants;
+import com.mrchandler.disableprox.util.FileUtil;
+
+import java.io.FileNotFoundException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+import static de.robv.android.xposed.XposedHelpers.findClass;
+
+public class XposedMod implements IXposedHookLoadPackage {
+
+    /**
+     * Shout out to abusalimov for his Light Sensor fix that inspired this app.
+     */
+    @Override
+    public void handleLoadPackage(final LoadPackageParam lpparam)
+            throws Throwable {
+        int enabledMethods = getMethodsUsedForDisabling();
+        if ((enabledMethods & Constants.ENABLE_METHOD_1) == Constants.ENABLE_METHOD_1) {
+            disableSystemSensorManager(lpparam);
+        }
+        //noinspection StatementWithEmptyBody
+        if ((enabledMethods & Constants.ENABLE_METHOD_2) == Constants.ENABLE_METHOD_2) {
+            //disableSensorEventListeners(lpparam);
+        }
+        if ((enabledMethods & Constants.ENABLE_METHOD_3) == Constants.ENABLE_METHOD_3) {
+            removeProximitySensor(lpparam);
+        }
+    }
+
+    /**
+     * Disable by changing the data that the SensorManager gives listeners.
+     **/
+    void disableSystemSensorManager(LoadPackageParam lpparam) {
+        // Alright, so we start by creating a reference to the class that handles sensors.
+        final Class<?> systemSensorManager = findClass(
+                "android.hardware.SystemSensorManager", lpparam.classLoader);
+
+        // Here, we grab the method that actually dispatches sensor events to tweak what it receives. Since the API seems to have changed in
+        // Jelly Bean MR2, we use two different method hooks depending on the API.
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
+
+            //This seems to work fine, but there might be a better method to override.
+            XposedHelpers.findAndHookMethod(
+                    "android.hardware.SystemSensorManager$ListenerDelegate",
+                    lpparam.classLoader, "onSensorChangedLocked", Sensor.class,
+                    float[].class, long[].class, int.class, new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) {
+                            //False == Turn Off, True == Leave On
+                            boolean proximitySensorStatus = getProximitySensorStatus();
+                            // This method receives the sensor that needs to be changed directly, making this code simpler than the JB MR2+ code.
+                            if (!proximitySensorStatus) { //This seems kinda weird but the proximity_enabled says whether the proximity sensor should be allowed to be enabled or not.
+                                Sensor s = (Sensor) param.args[0];
+                                if (s.getType() != Sensor.TYPE_PROXIMITY) {
+                                    return;
+                                }
+                                // So we grab that float[], set the maximum for the Proximity Sensor, and call it a day.
+                                float[] values = (float[]) param.args[1];
+                                values[0] = s.getMaximumRange();
+                                param.args[1] = values;
+                            }
+                        }
+                    }
+            );
+        } else {
+            XC_MethodHook oldProximityHook = new XC_MethodHook() {
+                @SuppressWarnings("unchecked")
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param)
+                        throws Throwable {
+                    boolean proximitySensorStatus = getProximitySensorStatus();
+
+                    if (!proximitySensorStatus) {
+                        // This pulls the 'Handle to Sensor' array straight from the SystemSensorManager class, so it should always pull the appropriate sensor.
+                        SparseArray<Sensor> sensors;
+                        //Marshmallow converted our field into a module level one, so we have different code based on that. Otherwise, the same.
+                        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+                            sensors = (SparseArray<Sensor>) XposedHelpers.getStaticObjectField(systemSensorManager, "sHandleToSensor");
+                        } else {
+                            Object systemSensorManager = XposedHelpers.getObjectField(param.thisObject, "mManager");
+                            sensors = (SparseArray<Sensor>) XposedHelpers.getObjectField(systemSensorManager, "mHandleToSensor");
+                        }
+
+                        // params.args[] is an array that holds the arguments that dispatchSensorEvent received, which are a handle pointing to a sensor
+                        // in sHandleToSensor and a float[] of values that should be applied to that sensor.
+                        int handle = (Integer) (param.args[0]); // This tells us which sensor was currently called.
+                        Sensor sensor = sensors.get(handle);
+                        if (sensor.getType() == Sensor.TYPE_PROXIMITY) {
+                            float[] values = (float[]) param.args[1];
+                            for (int i = 0; i < values.length; i++) {
+                                //Even though only values[0] is checked, we do this just in case.
+                                //I worry that sensor.getMaximumRange() might not always be set, but I've seen no instance of that case.
+                                values[i] = sensor.getMaximumRange();
+                            }
+                            param.args[1] = values;
+                        }
+                    }
+                }
+            };
+            XposedHelpers.findAndHookMethod("android.hardware.SystemSensorManager$SensorEventQueue", lpparam.classLoader, "dispatchSensorEvent", int.class, float[].class, int.class, long.class, oldProximityHook);
+        }
+    }
+
+    /**
+     * Method 2: Disable by changing the values that Listeners receive via delegating to the original listener.
+     * [Probably the worst way to go about it.]
+     **/
+    void disableSensorEventListeners(LoadPackageParam lpparam) {
+
+        XC_MethodHook method2RegisterHook = new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                Sensor sensor = (Sensor) param.args[1];
+                if (!getProximitySensorStatus() && sensor.getType() == Sensor.TYPE_PROXIMITY) {
+                    final SensorEventListener oldListener = (SensorEventListener) param.args[0];
+                    if (oldListener instanceof InjectedSensorEventListener) {
+                        return;
+                    }
+                    InjectedSensorEventListener injectedListener = new InjectedSensorEventListener(oldListener);
+                    param.args[0] = injectedListener;
+                }
+            }
+        };
+
+        XC_MethodHook method2UnregisterHook = new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                //TODO The problem with this method is it ruins the unregister. It could be possible
+                //if there was an easier way to know if the listener received here had been shadowed
+                //by an InjectedSensorEventListener. I leave it in as a possibility, but this is not
+                //used.
+            }
+        };
+        XposedHelpers.findAndHookMethod("android.hardware.SystemSensorManager", lpparam.classLoader, "registerListenerImpl", SensorEventListener.class, Sensor.class, int.class, Handler.class, int.class, int.class, method2RegisterHook);
+        XposedHelpers.findAndHookMethod("android.hardware.SystemSensorManager", lpparam.classLoader, "unregisterListenerImpl", SensorEventListener.class, Sensor.class, method2UnregisterHook);
+    }
+
+    /**
+     * Disable by removing the sensor data from the SensorManager. Apps will think the sensor does not exist.
+     **/
+    void removeProximitySensor(LoadPackageParam lpparam) {
+        //This is the base method that gets called whenever the sensors are queryed. All roads lead back to getFullSensorList!
+        XposedHelpers.findAndHookMethod("android.hardware.SystemSensorManager", lpparam.classLoader, "getFullSensorList", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                if (!getProximitySensorStatus()) {
+                    List<Sensor> fullSensorList = (List<Sensor>) param.getResult();
+                    Iterator<Sensor> iterator = fullSensorList.iterator();
+                    while (iterator.hasNext()) {
+                        Sensor sensor = iterator.next();
+                        if (sensor.getType() == Sensor.TYPE_PROXIMITY) {
+                            iterator.remove();
+                        }
+                    }
+                    param.setResult(fullSensorList);
+                }
+            }
+        });
+    }
+
+    boolean getProximitySensorStatus() {
+        //False == Turn Off, True == Leave On
+        //Always assume that the user wants it disabled. They can disable the app if we fail somehow.
+        boolean proximitySensorStatus = false;
+        try {
+            Scanner scanner = new Scanner(FileUtil.getEnabledSettingsFile());
+            if (scanner.hasNextBoolean()) {
+                proximitySensorStatus = scanner.nextBoolean();
+            }
+        } catch (FileNotFoundException e) {
+            //We do nothing and assume that the proximity sensor should be disabled.
+        }
+        return proximitySensorStatus;
+    }
+
+    //TODO Implement a visual system for this.
+    int getMethodsUsedForDisabling() {
+        int enabledMethods = Constants.ENABLE_METHOD_1;
+        try {
+            Scanner scanner = new Scanner(FileUtil.getEnabledMethodsFile());
+            if (scanner.hasNextInt()) {
+                enabledMethods = scanner.nextInt();
+            }
+        } catch (FileNotFoundException e) {
+            //We do nothing and use the default method.
+        }
+        return enabledMethods;
+    }
+
+    /**
+     * Used for delegating a false Sensor value to registered sensor listeners.
+     **/
+    class InjectedSensorEventListener implements SensorEventListener {
+
+        SensorEventListener oldListener;
+
+        public InjectedSensorEventListener(SensorEventListener oldListener) {
+            this.oldListener = oldListener;
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            event.values[0] = event.sensor.getMaximumRange();
+            oldListener.onSensorChanged(event);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            oldListener.onAccuracyChanged(sensor, accuracy);
+        }
+    }
+}
---
+++ app/src/main/java/com/mrchandler/disableprox/ui/EditActivity.java
@@ -0,0 +1,269 @@
+/*
+ * Copyright 2013 two forty four a.m. LLC <http://www.twofortyfouram.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * <http://www.apache.org/licenses/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package com.mrchandler.disableprox.ui;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.mrchandler.disableprox.R;
+import com.mrchandler.disableprox.bundle.BundleScrubber;
+import com.mrchandler.disableprox.bundle.PluginBundleManager;
+import com.mrchandler.disableprox.util.Constants;
+import com.mrchandler.disableprox.util.IabHelper;
+import com.mrchandler.disableprox.util.IabResult;
+import com.mrchandler.disableprox.util.Inventory;
+import com.mrchandler.disableprox.util.Purchase;
+
+/**
+ * This is the "Edit" activity for a Locale Plug-in.
+ * <p/>
+ * This Activity can be started in one of two states:
+ * <ul>
+ * <li>New plug-in instance: The Activity's Intent will not contain
+ * {@link com.twofortyfouram.locale.Intent#EXTRA_BUNDLE}.</li>
+ * <li>Old plug-in instance: The Activity's Intent will contain
+ * {@link com.twofortyfouram.locale.Intent#EXTRA_BUNDLE} from a previously saved plug-in instance that the
+ * user is editing.</li>
+ * </ul>
+ *
+ * @see com.twofortyfouram.locale.Intent#ACTION_EDIT_SETTING
+ * @see com.twofortyfouram.locale.Intent#EXTRA_BUNDLE
+ */
+public final class EditActivity extends AbstractPluginActivity {
+
+    private static final String TAG = EditActivity.class.getSimpleName();
+    private static final int PURCHASE_RESULT_CODE = 191;
+    private static final int PERMISSION_RESULT_CODE = 192;
+
+    private boolean doNotSave = false;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        BundleScrubber.scrub(getIntent());
+
+        final Bundle localeBundle = getIntent().getBundleExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE);
+        BundleScrubber.scrub(localeBundle);
+
+        setContentView(R.layout.main);
+
+        if (null == savedInstanceState) {
+            if (PluginBundleManager.isBundleValid(localeBundle)) {
+                final boolean setting =
+                        localeBundle.getBoolean(PluginBundleManager.BUNDLE_EXTRA_BOOLEAN_SETTING);
+                ((Switch) findViewById(android.R.id.checkbox)).setChecked(setting);
+            }
+        }
+        final IabHelper helper = new IabHelper(this, getString(R.string.google_billing_public_key));
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Has the user purchased the Tasker IAP?
+        if (!prefs.getBoolean(Constants.PREFS_KEY_TASKER, false)) {
+            helper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+                @Override
+                public void onIabSetupFinished(IabResult result) {
+                    if (result.isFailure()) {
+                        Log.d(TAG, "Unable to get up In-App Billing. Oh well.");
+                        queryPermission();
+                        return;
+                    }
+                    helper.queryInventoryAsync(new IabHelper.QueryInventoryFinishedListener() {
+                        @Override
+                        public void onQueryInventoryFinished(IabResult result, Inventory inv) {
+                            if (result.isFailure()) {
+                                prefs.edit().putBoolean(Constants.PREFS_KEY_TASKER, false).apply();
+                                queryPermission();
+                                return;
+                            }
+                            if (inv.hasPurchase(Constants.SKU_TASKER)) {
+                                prefs.edit().putBoolean(Constants.PREFS_KEY_TASKER, true).apply();
+                                prefs.edit().remove(Constants.PREFS_KEY_FREELOAD).apply();
+                                queryPermission();
+                            } else {
+                                prefs.edit().putBoolean(Constants.PREFS_KEY_TASKER, false).apply();
+                                if (!(prefs.contains(Constants.PREFS_KEY_FREELOAD) && prefs.getBoolean(Constants.PREFS_KEY_FREELOAD, false))) {
+                                    AlertDialog dialog = new AlertDialog.Builder(EditActivity.this)
+                                            .setTitle(R.string.iap_dialog_title)
+                                            .setMessage(R.string.iap_dialog_message)
+                                            .setNegativeButton("Not Right Now", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    doNotSave = true;
+                                                    finish();
+                                                }
+                                            })
+                                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    helper.launchPurchaseFlow(EditActivity.this, Constants.SKU_TASKER, PURCHASE_RESULT_CODE, new IabHelper.OnIabPurchaseFinishedListener() {
+                                                        @Override
+                                                        public void onIabPurchaseFinished(IabResult result, Purchase info) {
+                                                            if (result.isFailure()) {
+                                                                Toast.makeText(EditActivity.this, "Error getting purchase details.", Toast.LENGTH_SHORT).show();
+                                                                doNotSave = true;
+                                                                finish();
+                                                                return;
+                                                            }
+                                                            if (info.getSku().equals(Constants.SKU_TASKER)) {
+                                                                prefs.edit().putBoolean(Constants.PREFS_KEY_TASKER, true).apply();
+                                                                queryPermission();
+                                                            }
+                                                        }
+                                                    });
+                                                }
+                                            })
+                                            .setNeutralButton("More Information", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    AlertDialog oldDialog = (AlertDialog) dialog;
+                                                    oldDialog.dismiss();
+                                                    AlertDialog newDialog = new AlertDialog.Builder(EditActivity.this)
+                                                            .setTitle(getString(R.string.iap_dialog_title))
+                                                            .setMessage(getString(R.string.iap_dialog_more_information))
+                                                            .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                                                                @Override
+                                                                public void onCancel(DialogInterface dialog) {
+                                                                    doNotSave = true;
+                                                                    finish();
+                                                                }
+                                                            })
+                                                            .setCancelable(true)
+                                                            .create();
+                                                    newDialog.setCanceledOnTouchOutside(true);
+                                                    newDialog.show();
+                                                }
+                                            })
+                                            .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                                                @Override
+                                                public void onCancel(DialogInterface dialog) {
+                                                    doNotSave = true;
+                                                    finish();
+                                                }
+                                            })
+                                            .setCancelable(true)
+                                            .create();
+                                    dialog.setCanceledOnTouchOutside(true);
+                                    dialog.show();
+                                } else {
+                                    queryPermission();
+                                }
+                            }
+                        }
+                    });
+                }
+            });
+        } else {
+            queryPermission();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case PURCHASE_RESULT_CODE:
+                if (resultCode != RESULT_OK) {
+                    Toast.makeText(this, "Error making purchase.", Toast.LENGTH_SHORT).show();
+                    doNotSave = true;
+                    finish();
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+                break;
+        }
+    }
+
+    @Override
+    public void finish() {
+        if (!isCanceled() && !doNotSave) {
+            final boolean setting = ((Switch) findViewById(android.R.id.checkbox)).isChecked();
+
+            final Intent resultIntent = new Intent();
+
+                /*
+                 * This extra is the data to ourselves: either for the Activity or the BroadcastReceiver. Note
+                 * that anything placed in this Bundle must be available to Locale's class loader. So storing
+                 * String, int, and other standard objects will work just fine. Parcelable objects are not
+                 * acceptable, unless they also implement Serializable. Serializable objects must be standard
+                 * Android platform objects (A Serializable class private to this plug-in's APK cannot be
+                 * stored in the Bundle, as Locale's classloader will not recognize it).
+                 */
+            final Bundle resultBundle =
+                    PluginBundleManager.generateBundle(getApplicationContext(), setting);
+            resultIntent.putExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE, resultBundle);
+
+                /*
+                 * The blurb is concise status text to be displayed in the host's UI.
+                 */
+            final String blurb = generateBlurb(setting);
+            resultIntent.putExtra(com.twofortyfouram.locale.Intent.EXTRA_STRING_BLURB, blurb);
+
+            setResult(RESULT_OK, resultIntent);
+
+        }
+
+        super.finish();
+    }
+
+    /**
+     * @param setting The new setting for the Proximity sensor.
+     * @return A blurb for the plug-in.
+     */
+    /* package */
+    static String generateBlurb(final boolean setting) {
+        return setting ? "Enabled" : "Disabled";
+    }
+
+    void queryPermission() {
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+                new AlertDialog.Builder(this)
+                        .setMessage("In order to allow Tasker the ability to change the proximity sensor setting, this app needs to save the settings to a file. \n\nWill you allow this app to save data on this phone's storage? Without this, Tasker will not work.")
+                        .setPositiveButton("Yes, that's fine.", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions(EditActivity.this,
+                                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                        PERMISSION_RESULT_CODE);
+                            }
+                        })
+                        .setNegativeButton("No, I'd rather not.", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .setCancelable(false)
+                        .create().show();
+            } else {
+                //We can ignore the result code since this Activity doesn't actually need it.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        PERMISSION_RESULT_CODE);
+            }
+        }
+    }
+}
