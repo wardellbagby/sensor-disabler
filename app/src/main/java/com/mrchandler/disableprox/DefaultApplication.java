1547da10e19670e28dd9aa5f051b785643b7ef9c
Wardell Chandler <wardell.chandler@gmail.com>
2016-01-29 15:56:22 -0600

    Adds in upgrade handling logic for upgrading from Disable Proximity Sensor

---
+++ app/src/main/java/com/mrchandler/disableprox/DefaultApplication.java
@@ -0,0 +1,32 @@
+package com.mrchandler.disableprox;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+
+import com.mrchandler.disableprox.util.Constants;
+import com.mrchandler.disableprox.util.SensorUtil;
+
+/**
+ * @author Wardell
+ */
+public class DefaultApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SharedPreferences prefs = getSharedPreferences(Constants.PREFS_FILE_NAME, MODE_WORLD_READABLE);
+        if (prefs.getBoolean("prefs_key_prox_sensor", false)) {
+            //Updating from an older version. Let's make sure the app will still work as intended.
+            SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+            Sensor proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+            String sensorKey = SensorUtil.generateUniqueSensorKey(proximitySensor);
+            String mockValuesKey = SensorUtil.generateUniqueSensorMockValuesKey(proximitySensor);
+            prefs.edit()
+                    .putInt(sensorKey, Constants.SENSOR_STATUS_MOCK_VALUES)
+                    .putString(mockValuesKey, "0:")
+                    .remove("prefs_key_prox_sensor")
+                    .apply();
+        }
+    }
+}
